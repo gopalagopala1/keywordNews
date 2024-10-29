698005abff4c7bbdf53f586754e8c1d606216a49
Gopala Agrawal <agrawalgopala@gmail.com>
2024-10-29 18:52:42 +0530

    feat: add logo and improve card

--- client/src/features/NewsCard/NewsCard.tsx
+++ client/src/features/NewsCard/NewsCard.tsx
@@ -1,7 +1,54 @@
-import { Card } from "@chakra-ui/react";
+import { NewsCardProps } from "@/types/news";
+import {
+  Box,
+  Card,
+  Flex,
+  Heading,
+  Text
+} from "@chakra-ui/react";
+import Image from "next/image";
 
-const NewsCard = () => {
-  return <Card></Card>;
+const NewsCard = ({
+  title,
+  description,
+  url,
+  image,
+  isLoading,
+}: NewsCardProps) => {
+  return (
+    <Card
+      w={{ base: "100%", md: "60%" }}
+      h={{ base: "auto", md: "15rem" }}
+      shadow="lg"
+      _hover={{ transform: "scale(1.02)", transition: "transform 0.2s" }}
+      as="article"
+    >
+      <Flex padding="0.5rem" h="100%" gap={4}>
+        <Box position="relative" w={{ base: "30%", md: "30%" }} minW="100px">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            style={{ objectFit: "cover" }}
+            priority
+          />
+        </Box>
+        <Flex flexDirection="column" flex={1} gap="0.5rem">
+          <Heading
+            size="md"
+            as="a"
+            href={url}
+            _hover={{ color: "blue.500" }}
+            cursor="pointer"
+          >
+            {title}
+          </Heading>
+
+          <Text noOfLines={3}>{description}</Text>
+        </Flex>
+      </Flex>
+    </Card>
+  );
 };
 
 export default NewsCard;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,47 @@
+import NewsCard from "@/features/NewsCard/NewsCard";
 import { Button, Flex, Text } from "@chakra-ui/react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <Flex justify="center" align="center" h="5rem" shadow="lg">
+    <Flex flexDirection="column" gap="1rem">
+      <Flex justify="center" align="center" h="5rem" shadow="lg" gap="0.1rem">
+        <Image src="/logo.webp" alt="Keyword News" width={50} height={50} />
         <Text fontSize="1.5rem" fontWeight="bold" fontStyle="italic">
           Keyword News
         </Text>
       </Flex>
 
-      <Flex>
-        <Button>Button</Button>
+      <Flex justify="center" align="center" w="100%" gap="1rem" direction='column'>
+        <NewsCard
+          title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          url="https://www.google.com"
+          image="https://via.placeholder.com/150"
+          isLoading={true}
+        />
+        <NewsCard
+          title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          url="https://www.google.com"
+          image="https://via.placeholder.com/150"
+          isLoading={true}
+        />{" "}
+        <NewsCard
+          title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          url="https://www.google.com"
+          image="https://via.placeholder.com/150"
+          isLoading={true}
+        />{" "}
+        <NewsCard
+          title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+          url="https://www.google.com"
+          image="https://via.placeholder.com/150"
+          isLoading={true}
+        />
       </Flex>
-    </>
+    </Flex>
   );
 }
