cf77cace9859fa8b887770e58fa8918432a63357
Gopala Agrawal <agrawalgopala@gmail.com>
2024-12-04 09:32:08 +0530

    fix: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import { TbVirusSearch } from "react-icons/tb";
-
 
 type HeaderProps = {
   isHomePage?: boolean;
@@ -10,32 +9,44 @@ type HeaderProps = {
 
 const Header = ({ isHomePage }: HeaderProps) => {
   return (
-    <Flex h="5rem" gap="0.1rem" position="fixed">
-      <Flex justify="space-between" align="center" h="100%" w="full">
-        <Flex align="center" gap="0.1rem" h="100%" justify="start">
-          <Image src="/logo_1.png" alt="Keyword News" width={40} height={40} />
+    <Flex
+      h="5rem"
+      gap="0.1rem"
+      position="fixed"
+      borderBottom="1px"
+      w="container.xl"
+      bg="white" 
+      zIndex="1000" 
+      top="0" 
+    >
+      <Flex justify="space-between" align="center" h="100%" w="full" >
+        <Link href="/">
+          <Flex align="center" gap="0.1rem" h="100%" justify="start">
+            <Image
+              src="/logo_1.png"
+              alt="Keyword News"
+              width={40}
+              height={40}
+            />
 
-          <Text
-            fontSize="1.5rem"
-            fontWeight="extrabold"
-            textTransform="uppercase"
-          >
-            Keyword News
-          </Text>
-        </Flex>
-        <TbVirusSearch size="40px" color="purple"/>
-        {/* {isHomePage && (
-          <Link href="/read">
             <Text
-              fontSize="1rem"
-              fontWeight="light"
-              textDecorationLine="underline"
-              textUnderlineOffset="0.25rem"
+              fontSize="1.5rem"
+              fontWeight="extrabold"
+              textTransform="uppercase"
             >
+              Keyword News
+            </Text>
+          </Flex>
+        </Link>
+        {isHomePage ? (
+          <Link href="/read">
+            <Text fontSize="1rem" color="purple">
               Read
             </Text>
           </Link>
-        )} */}
+        ) : (
+          <TbVirusSearch size="2rem" color="purple" />
+        )}
       </Flex>
     </Flex>
   );
