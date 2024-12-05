12ffaaa6cc8986b296602193e36627ffa5c3cef3
Gopala Agrawal <agrawalgopala@gmail.com>
2024-12-05 22:52:37 +0530

    fix: search

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -20,23 +20,29 @@ const Search = ({ isOpen, onClose }: SearchType) => {
       <ModalOverlay />
 
       <ModalContent>
-        <Flex direction="column" gap="1rem" width="full" bg="white">
-          <InputGroup flexDirection="column">
-            <Text fontSize="1rem">Include Keywords:</Text>
-            <Input />
-            <Text fontSize="0.5rem" fontStyle="italic">
-              Search for news with space or comma separated keywords
-            </Text>
-          </InputGroup>
+        <ModalBody padding="2rem">
+          <Flex direction="column" gap="1rem" width="full" bg="white">
+            <InputGroup flexDirection="column">
+              <Text fontSize="0.8rem" mb="0.5rem" fontWeight="600">
+                Include Keywords
+              </Text>
+              <Input />
+              <Text fontSize="0.5rem" fontStyle="italic" color="grey">
+                Search for news with space or comma separated keywords
+              </Text>
+            </InputGroup>
 
-          <InputGroup flexDirection="column">
-            <Text>Exclude Keywords:</Text>
-            <Input />
-            <Text fontSize="0.5rem" fontStyle="italic">
-              Search for news without space or comma separated keywords
-            </Text>
-          </InputGroup>
-        </Flex>
+            <InputGroup flexDirection="column">
+              <Text fontSize="0.8rem" mb="0.5rem" fontWeight="600">
+                Exclude Keywords
+              </Text>
+              <Input />
+              <Text fontSize="0.5rem" fontStyle="italic" color="grey">
+                Search for news without space or comma separated keywords
+              </Text>
+            </InputGroup>
+          </Flex>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
