7334b1eb592e4b9331e9f29fcf416bfd021db95b
Gopala Agrawal <agrawalgopala@gmail.com>
2024-12-24 00:47:08 +0530

    feat: mobile front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,40 @@
 import Header from "@/components/Header";
+import { useDeviceSizes } from "@/hooks/useDeviceSizes";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <Header isHomePage={true} />
-      <Flex
-        gap="0.5rem"
-        width="full"
-        height="full"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Flex width="80%" textAlign="center" direction="column" gap="2rem">
-          <Text
-            fontSize="3rem"
-            color="purple.700"
-            fontWeight="thin"
-            textTransform="capitalize"
-          >
-            Stay informed, keyword way
-          </Text>
+  const { isMobile } = useDeviceSizes();
 
-          <Text fontSize="1rem" fontWeight="light">
-            With Keyword News, discover news tailored to your interests. Search
-            for news by including the topics you care about—or{" "}
-            <strong>exclude</strong> keywords to filter out what you don’t.
-            It&apos;s news, customized to you.
-          </Text>
-        </Flex>
+  const heroText = () => (
+    <Box bg="red.400" width="full" height="62vh" position="relative">
+      <Text
+        fontSize={isMobile ? "2.5rem" : "3rem"}
+        color="white"
+        mt="8rem"
+        textAlign="center"
+        textTransform="uppercase"
+        fontWeight="bold"
+      >
+        Stay Informed,
+        <br /> Keyword Way
+      </Text>
+    </Box>
+  );
+
+  return (
+    <Flex direction="column" width="full" height="full" position="relative">
+      <Header isHomePage={true} />
+      {heroText()}
+
+      <Flex textAlign="center" direction="column" mt="2rem">
+        <Text fontSize="1.25rem"  textAlign="start">
+          With Keyword News, discover news tailored to your interests. Search
+          for news by including the topics you care about—or{" "}
+          <strong>exclude</strong> keywords to filter out what you don’t.
+          It&apos;s news, customized to you.
+        </Text>
       </Flex>
-    </>
+    </Flex>
   );
 }
